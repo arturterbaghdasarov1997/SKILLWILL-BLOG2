97f78c3e9ad08fecad71444f68b8bc5049cff830
unknown <terbagdasarovartur@gmail.com>
2024-02-13 19:31:46 +0400

    First commit

---
+++ variables.php
@@ -0,0 +1,27 @@
+<?php
+      
+    $projects = [
+        'title' => 'project1',
+        'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum. <br> Nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim.</p>',
+        'image' => '<img src="./Assets/Card-Image1.png" alt="">',
+    ];
+
+    $projects = [
+        [
+            'title' => 'project1',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum. <br> Nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim.</p>',  
+            'image' => 'Assets/Post1.png'
+        ],
+        [
+            'title' => 'project2',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum. <br> Nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim.</p>',  
+            'image' => 'Assets/Post2.png'
+        ],
+
+        [
+            'title' => 'project3',
+            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum. <br> Nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim.</p>',  
+            'image' => 'Assets/Post3.png'
+        ],
+    ];
+?>
